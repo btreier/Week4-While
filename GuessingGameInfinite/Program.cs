693ba6d2cde1634cb2b4593a2b79dbbc3c65ce68
Birgith Maria Treier <btreier@tktk.ee>
2021-11-21 12:36:42 +0200

    add new projects

---
+++ GuessingGameInfinite/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace GuessingGameInfinite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm genereerib juhuslikku numbrit 1 kuni 10;
+            //kasutaja peab seda numbrit ära arvama;
+            //kui kasutaja suutis seda numbrit ära arvata, siis ta on  mängu võitnud;
+            //katsete arv on piiramatu
+            //*programm genereerib juhuslikku numbrit vaid ühte korda
+
+            Random rnd = new Random();
+            int cpuNumber = rnd.Next(1, 11);
+            bool loopActive = true;
+            int i = 0;
+
+            while (loopActive)
+            {
+                Console.WriteLine("Arva numbrit 1 kuni 10:");
+                int userGuess = Convert.ToInt32(Console.ReadLine());
+                i++;
+
+                if (userGuess == cpuNumber)
+                {
+                    Console.WriteLine("Palju õnne, sa arvasid õigesti!"); ;
+                    loopActive = false;
+                }
+                else 
+                {
+                    Console.WriteLine($"Oled arvanud {i} korda valesti.");
+                }
+            }
+        }
+    }
+}
---
+++ GuessingGameThreeTries/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace GuessingGameThreeTries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm genereerib juhuslikku numbrit 1 kuni 10;
+            //kasutaja peab seda numbrit ära arvama;
+            //kui kasutaja suutis seda numbrit ära arvata, siis ta on  mängu võitnud;
+            //kasutajal on kolm katset, kui kasutaja ei ole suutnud kolme katsega numbrit ära arvata 
+            //siis mängu võidab arvuti
+            //*programm genereerib juhuslikku numbrit vaid ühte korda
+
+            Random rnd = new Random();
+            int cpuNumber = rnd.Next(1, 11);
+            int i = 0;
+
+            while(i < 3)
+            {
+                Console.WriteLine("Arva numbrit 1 kuni 10:");
+                int userGuess = Convert.ToInt32(Console.ReadLine());
+                i++;
+                if (userGuess == cpuNumber)
+                {
+                    Console.WriteLine("Palju õnne, sa arvasid õigesti!");
+                    break;
+                }
+                Console.WriteLine($"Vale number. Sul on jäänud {3 - i} katset.");
+                
+                if (i == 3)
+                {
+                    Console.WriteLine("Sinu katsete arv on täis. Arvuti võitis seekord.");
+                }
+               
+                
+            }
+
+        }
+    }
+}
